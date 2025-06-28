56b8cfc86e10c3b88962c3841860b45fc8b70ad4
Pijar Adiluhung <pijaradiluhung101@gmail.com>
2025-06-28 19:58:25 +0700

    fixed for ios

---
+++ tasbih/tasbih.js
@@ -0,0 +1,36 @@
+// Initialize variables
+let tasbih = 0;
+const tasbihDisplay = document.getElementById("tasbih-count");
+const tasbihAddButton = document.getElementById("tasbih-add");
+const tasbihResetButton = document.getElementById("tasbih-reset");
+const savedTasbih = localStorage.getItem("savedCount");
+
+// Get local storage
+if (savedTasbih !== null) {
+  tasbihDisplay.textContent = Number(savedTasbih);
+  tasbih = Number(savedTasbih);
+} else {
+  tasbih = 0;
+}
+
+// Function to add and reset, with updating local storage
+function tasbihAdd() {
+  if ("vibrate" in navigator) {
+        navigator.vibrate(200); 
+  }
+  tasbih = tasbih + 1;
+  tasbihDisplay.textContent = tasbih;
+  localStorage.setItem("savedCount", tasbih);
+}
+function tasbihReset() {
+  if ("vibrate" in navigator) {
+        navigator.vibrate(500); 
+  }
+  tasbih = 0;
+  tasbihDisplay.textContent = tasbih;
+  localStorage.setItem("savedCount", 0);
+}
+
+// Add event listeners to buttons
+tasbihAddButton.addEventListener("click", tasbihAdd);
+tasbihResetButton.addEventListener("click", tasbihReset);
